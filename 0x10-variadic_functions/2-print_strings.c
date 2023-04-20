134301e3932a3f8f6650379fb6d9898a3930ec69
Leonard-Nfor <leonardnforkort@.com>
2023-04-20 14:25:09 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,9 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(strings, n);
-	i = 0;
-	while (i < n)
-		i++;
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
 		if (str == NULL)
