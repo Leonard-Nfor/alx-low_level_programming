cdf2591853b27434b9ede55267844314ef4466b7
Leonard-Nfor <leonardnforkort@gmail.com>
2023-06-16 16:20:24 -0700

    memset is used

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * _memset - Functions name
+ * @s: pointer
+ * b: character to initialized
+ * @n: the size in bytes
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+	return (s);
+}
+/**
+ * -calloc - the functions name;
+ *  @nmemb: elements
+ *  @size: size in bytes
+ *  Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	_memset(p, 0, nmemb * size);
+	return (p);
+}
