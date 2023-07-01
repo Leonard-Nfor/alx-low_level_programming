a64353d4de9c9de885dfb0abe2949078ebc4c5c0
Leonard-Nfor <leonardnforkort@gmail.com>
2023-07-01 14:06:25 -0700

    main

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,12 +4,15 @@
 *@h:pointer
 *                                                 *Return: the prented members
 */                                                size_t print_list(const list_t *h)                {
-        size_t j = 0;
 
-        while (h)
-        {
-                if (!h->str)                                              printf("[0] (nil)\n");
-		else
+size_t j = 0;
+
+while (h)
+
+{
+
+if (!h->str)                                              printf("[0] (nil)\n");
+else
 printf("[%u]%s\n", h->len, h->str);
 h = h->next;
 j++;
