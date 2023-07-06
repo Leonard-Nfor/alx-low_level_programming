a103962b9cf97106dc301c79e9649fc93ede1a8e
Leonard-Nfor <leonardnforkort@gmail.com>
2023-07-05 17:29:43 -0700

    means

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * _strdup - return an address fill with strs
+ * @str: The string to be filled
+ * Return: The address
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	ptr = malloc(sizeof(char) * (i + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		ptr[r] = str[r];
+	return (ptr);
+}
