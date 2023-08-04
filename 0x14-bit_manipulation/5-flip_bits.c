da28ac23c2103a2b16638d670399ecd31466542c
Leonard-Nfor <leonardnforkort@gmail.com>
2023-08-04 04:09:09 -0700

    much

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,8 +9,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int cur_value;
+
 	unsigned long int exclu = n ^ m;
-	
+
 	for (i = 63; i >= 0; i--)
 	{
 		cur_value = exclu >> i;
